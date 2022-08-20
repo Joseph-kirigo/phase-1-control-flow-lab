e36145b52cd669c191f5089380428171a6613474
Joseph-kirigo <josephkirigo777@gmail.com>
2022-08-20 08:16:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(checkCity){
   // Write your code here!
+  if (checkCity==='NYC'){
+    return "Ok, sounds good."
+  }
+  else if(checkCity!='NYC'){
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return 'Thank you so much.';
+    case "not as generous":
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
 }
